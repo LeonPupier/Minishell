f8263965061928fec2eb8f869233dad9ea4c4c80
Valentin Cart <vcart@z3r8p5.42lyon.fr>
2023-02-21 15:34:35 +0100

    multiple pipe handling

--- sources/ft_pipe.c
+++ sources/ft_pipe.c
@@ -3,69 +3,100 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pipe.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpupier <lpupier@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: vcart < vcart@student.42lyon.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 12:16:45 by vcart             #+#    #+#             */
-/*   Updated: 2023/02/20 15:18:14 by lpupier          ###   ########.fr       */
+/*   Updated: 2023/02/21 15:28:20 by vcart            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int exec_first_cmd(int *fd, char **cmd_1, char **envp, t_list *new_envp)
+// Ferme les deux extrémités du pipe
+void close_pipe(int *fd)
 {
-	int fd_infile;
-
-	fd_infile = open(cmd_1[1], O_RDONLY);
-	if (fd_infile == -1)
-		return(perror(cmd_1[0]), -1);
-	dup2(fd[1], STDOUT_FILENO);
-	dup2(fd_infile, STDIN_FILENO);
 	close(fd[0]);
 	close(fd[1]);
-	close(fd_infile);
-	check_functions(cmd_1, envp, new_envp);
+}
+
+// Modifie les deux valeurs d'un tableau de deux entiers
+void change_fd(int *fd, int value1, int value2)
+{
+	fd[0] = value1;
+	fd[1] = value2;
+}
+
+// Exécute une commande en redirigeant l'entrée et/ou la sortie
+int exec_cmd(int *prev_fd, int *next_fd, char **cmd, char **envp, t_list *new_envp)
+{
+	if (prev_fd != NULL)
+	{
+		dup2(prev_fd[0], STDIN_FILENO);
+		close(prev_fd[0]);
+		close(prev_fd[1]);
+	}
+	if (next_fd != NULL)
+	{
+		close(next_fd[0]);
+		dup2(next_fd[1], STDOUT_FILENO);
+		close(next_fd[1]);
+	}
+	check_functions(cmd, envp, new_envp); // Exécute la commande
 	return (0);
 }
 
-int exec_second_cmd(int *fd, char **cmd_2, char **envp, t_list *new_envp)
+// Gère l'exécution de toutes les commandes avec les pipes
+int handle_pipe(int num_pipes, int *prev_fd, int *curr_fd, char ***cmd_tab, char **envp, t_list *new_envp)
 {
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[0]);
-	close(fd[1]);
-	check_functions(cmd_2, envp, new_envp);
+	int i;
+	int pid;
+	
+	i = 0;
+	while (i < num_pipes)
+	{
+		if (i < num_pipes - 1)
+		{
+			if (pipe(curr_fd) == -1)
+				return (-1);
+		}
+		else
+			change_fd(curr_fd, -1, -1);
+		pid = fork(); // Crée un nouveau processus pour exécuter la commande
+		if (pid < 0)
+			return (-2);
+		if (pid == 0) // Si on est dans le processus fils
+		{
+			exec_cmd(prev_fd, curr_fd, cmd_tab[i], envp, new_envp); // Exécute la commande
+			exit(0); // Termine le processus fils
+		}
+		if (prev_fd[0] != STDIN_FILENO) // Ferme les extrémités du pipe du processus précédent s'il y en a un
+			close_pipe(prev_fd);
+		change_fd(prev_fd, curr_fd[0], curr_fd[1]);
+		i++;
+	}
 	return (0);
 }
 
+// Fonction principale pour l'exécution des commandes avec des pipes
 int ft_pipe(char ***cmd_tab, char **envp, t_list *new_envp)
 {
-	int fd[2];
-	int pid1;
-	int pid2;
-
-	if (pipe(fd) == -1)
-		return (-1);
-	pid1 = fork();
-	if (pid1 < 0)
-		return (-2);
-	if (pid1 == 0)
+	int num_pipes;
+	int i;
+	int prev_fd[2];
+	int curr_fd[2];
+	
+	num_pipes = 0;
+	prev_fd[0] = STDIN_FILENO;
+	prev_fd[1] = STDOUT_FILENO;
+	while (cmd_tab[num_pipes] != NULL)
+		num_pipes++;
+	handle_pipe(num_pipes, prev_fd, curr_fd, cmd_tab, envp, new_envp); 
+	close_pipe(prev_fd);
+	i = 0;
+	while (i < num_pipes)
 	{
-		if (exec_first_cmd(fd, cmd_tab[0], envp, new_envp) == -1)
-			exit(1);
-		exit(0);
+		wait(NULL);
+		i++;
 	}
-	pid2 = fork();
-	if (pid2 < 0)
-		return (-2);
-	if (pid2 == 0)
-	{
-		if (exec_second_cmd(fd, cmd_tab[1], envp, new_envp) == -1)
-			exit(1);
-		exit(0);
-	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
 	return (0);
 }
