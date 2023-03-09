1a60afb9daa1f2582d2e94522dabb3d5a2747c10
vcart <vcart@student.42lyon.fr>
2023-03-09 21:09:18 +0100

    heredoc fixed + infile & heredoc improved

--- sources/commands.c
+++ sources/commands.c
@@ -6,7 +6,7 @@
 /*   By: vcart <vcart@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 21:19:19 by lpupier           #+#    #+#             */
-/*   Updated: 2023/03/09 13:44:20 by vcart            ###   ########.fr       */
+/*   Updated: 2023/03/09 20:23:58 by vcart            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ int	check_functions(char **cmd, t_env *envi, int status)
 
 	if (!status && check_redirections(cmd))
 		make_redirections(cmd);
-	if (status == 2)
-		return (1);
 	builtins_exit = check_builtins(cmd, envi);
 	if (builtins_exit == -1)
 		return (EXIT_FAILURE);
@@ -38,7 +36,9 @@ int	check_functions(char **cmd, t_env *envi, int status)
 
 int	check_builtins(char **cmd, t_env *envi)
 {
-	if (!ft_strcmp(cmd[0], "echo"))
+	if (check_infiles(cmd))
+		handle_infiles(cmd, envi, 1);
+	else if (!ft_strcmp(cmd[0], "echo"))
 		echo(cmd);
 	else if (!ft_strcmp(cmd[0], "pwd"))
 		pwd(cmd, envi->envp);
@@ -52,8 +52,6 @@ int	check_builtins(char **cmd, t_env *envi)
 		ft_unset(cmd, envi->new_envp);
 	else if (!ft_strcmp(cmd[0], "exit"))
 		return (printf("exit\n"), -1);
-	else if (!ft_strcmp(cmd[0], "<") || !ft_strcmp(cmd[0], "<<"))
-		handle_infiles(cmd, envi, 1);
 	else
 		return (0);
 	return (1);
