2a07f2ca006b2975c8926987e384cfa6ba5558f7
vcart <vcart@student.42lyon.fr>
2023-03-26 20:48:37 +0200

    fixed cd

--- sources/builtins/cd/ft_cd.c
+++ sources/builtins/cd/ft_cd.c
@@ -6,13 +6,28 @@
 /*   By: vcart <vcart@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 18:14:36 by vcart             #+#    #+#             */
-/*   Updated: 2023/03/22 15:05:50 by vcart            ###   ########.fr       */
+/*   Updated: 2023/03/26 20:47:00 by vcart            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-static int	update_pwd(t_list *new_envp)
+int	check_same_path(t_env *envi)
+{
+	char	cwd[1024];
+	char	*oldpwd;
+
+	oldpwd = get_env(envi->envp, "PWD");
+	if (!oldpwd)
+		return (-1);
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		return (free(oldpwd), perror("getcwd"), -1);
+	if (ft_strcmp(cwd, oldpwd))
+		return (free(oldpwd), 1);
+	return (free(oldpwd), 0);
+}
+
+static int	update_pwd(t_env *envi)
 {
 	char	cwd[1024];
 	char	*cmd[3];
@@ -20,7 +35,7 @@ static int	update_pwd(t_list *new_envp)
 	char	*wd;
 
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
-		return (perror("getcwd"), 1);
+		return (perror("getcwd"), -1);
 	cmd[0] = "export";
 	pwd = ft_strdup("PWD=");
 	if (!pwd)
@@ -32,13 +47,13 @@ static int	update_pwd(t_list *new_envp)
 	if (!cmd[1])
 		return (free(pwd), free(wd), -1);
 	cmd[2] = NULL;
-	if (ft_export(cmd, new_envp) == -1)
-		return (-1);
+	if (ft_export(cmd, envi->new_envp) == -1)
+		return (free(pwd), free(wd), -1);
 	free(cmd[1]);
 	return (0);
 }
 
-int	cd(char **cmd, t_list *new_envp, char **envp)
+int	cd(char **cmd, t_env *envi)
 {
 	int		argc;
 	char	*home;
@@ -46,7 +61,7 @@ int	cd(char **cmd, t_list *new_envp, char **envp)
 	argc = get_array_size(cmd);
 	if (argc == 1)
 	{
-		home = get_env(envp, "HOME");
+		home = get_env(envi->envp, "HOME");
 		if (!home)
 			return (-1);
 		if (chdir(home) != 0)
@@ -57,5 +72,7 @@ int	cd(char **cmd, t_list *new_envp, char **envp)
 		return (printf("cd : too many arguments\n"), 1);
 	else if (chdir(cmd[1]) != 0)
 		return (perror("cd"), 1);
-	return (update_pwd(new_envp));
+	if (check_same_path(envi))
+		return (update_pwd(envi));
+	return (0);
 }
