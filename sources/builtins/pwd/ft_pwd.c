1f7855afd5e491409dbbb4104806137e2a962228
LeonPupier <leonpupier@gmail.com>
2023-04-04 20:25:42 +0200

    patch Makefile relink and delete comments on pwd

--- sources/builtins/pwd/ft_pwd.c
+++ sources/builtins/pwd/ft_pwd.c
@@ -6,18 +6,12 @@
 /*   By: lpupier <lpupier@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:35:40 by vcart             #+#    #+#             */
-/*   Updated: 2023/04/04 16:42:47 by lpupier          ###   ########.fr       */
+/*   Updated: 2023/04/04 20:14:54 by lpupier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-/**
- * @brief Print name of current/working directory.
- * 
- * @param cmd List of character strings representing.
- * @param envp Environment instructions.
- */
 int	pwd(char **cmd)
 {
 	char	*working_directory;
